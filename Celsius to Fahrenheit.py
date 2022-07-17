96abfacad4f573139e3cde8785c89a80200dd141
Bruno Gonçalves <106673552+BrunoGonSouza@users.noreply.github.com>
2022-07-17 11:24:02 -0300

    Add files via upload

---
+++ Celsius to Fahrenheit.py
@@ -0,0 +1,11 @@
+'''Exercicio 15'''
+
+#Entrada
+temperaturaCelsius = float(input("Digite a temperatura em graus Celsius (ex.: 30): "))
+
+
+#Processamento
+temperaturaFahrenheit = (9*temperaturaCelsius+160)/5
+
+#Saída
+print(f"Essa temperatura em Fahrenheit é de {temperaturaFahrenheit} graus")
